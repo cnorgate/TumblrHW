97d6c4be44d1daff824706586c6e5bb90db04f9b
cnorgate <cnorgate@tcc-cnorgate2.local>
2015-10-08 12:17:44 -0700

    Initial Commit

---
+++ TumblrHW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TumblrHW
+//
+//  Created by Cameron Norgate on 10/8/15.
+//  Copyright © 2015 Cameron Norgate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
